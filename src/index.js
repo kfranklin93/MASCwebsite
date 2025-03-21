496ab38db7c8b8b6afc06c612e2f04f8f07e8ce2
Kenan Franklin <kenanfranklin@OCTO-TC9VPMVGPM.octo.us>
2025-03-21 15:44:41 -0400

    firebase HOST changes

--- src/index.js
+++ src/index.js
@@ -26,7 +26,9 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
     <App />
   </BrowserRouter>
+  </React.StrictMode>
 );
