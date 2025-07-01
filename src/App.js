a18c0f603258e14b94dfe8c705ebeead087dcfd0
Kenan Franklin <kenanfranklin@OCTO-TC9VPMVGPM.octo.us>
2025-07-01 09:45:25 -0400

    reverted app and index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import { GlobalStyles } from './styles/GlobalStyles';
-import { PageWrapper, MainContent } from './components/layout/Layout';
-// Import your existing components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
+import Services from './components/Services';
+import ABATherapy from './components/ABATherapy';
+import Insurance from './components/Insurance';
+import WhatToExpect from './components/WhatToExpect';
 import Footer from './components/Footer';
+import Contact from './components/Contact';
+import HiringBanner from './components/HiringBanner';
+import NoWaitlistBadge from './components/NoWaitlistBadge';
+import HiringBadge from './components/HiringBadge';
+import VoteBanner from './components/VoteBanner';
+
+import { Routes, Route } from 'react-router-dom';
+
+// Import Service Pages
+import SpeechTherapy from './components/ServicePages/SpeechTherapy';
+import EarlyIntervention from './components/ServicePages/EarlyIntervention';
+import AutismDiagnostic from './components/ServicePages/AutismDiagnostic';
+import ABATherapyPage from './components/ServicePages/ABATherapy';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <PageWrapper>
-        <Navbar />
-        <MainContent>
-          <Hero />
-          <About />
-          {/* Other components */}
-        </MainContent>
-        <Footer />
-      </PageWrapper>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <HiringBanner />
+              <About />
+              <NoWaitlistBadge />
+              <Services />
+              <HiringBadge />
+              <Insurance />
+              <VoteBanner />
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/aba-therapy" element={<ABATherapy />} />
+        <Route path="/insurance" element={<Insurance />} />
+        <Route path="/what-to-expect" element={<WhatToExpect />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Service Pages Routes */}
+        <Route path="/speech-therapy" element={<SpeechTherapy />} />
+        <Route path="/early-intervention" element={<EarlyIntervention />} />
+        <Route path="/autism-diagnostic" element={<AutismDiagnostic />} />
+        <Route path="/aba-therapy-page" element={<ABATherapyPage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { GlobalStyles } from './styles/GlobalStyles';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyles />
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
